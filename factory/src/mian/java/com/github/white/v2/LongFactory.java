6fd0a498bea98eacc33f8a93bf658b631ea12864
white <294487387@qq.com>
2021-04-16 11:48:23 +0800

    添加注释。
    呀呀呀，几天没提交了

--- factory/src/mian/java/com/github/white/v2/LongFactory.java
+++ factory/src/mian/java/com/github/white/v2/LongFactory.java
@@ -2,8 +2,17 @@ package com.github.white.v2;
 
 import java.util.Objects;
 
+/**
+ * The type Long factory.
+ */
 public class LongFactory implements NumberFactory {
 
+    /**
+     * Parse number.
+     *
+     * @param str the str
+     * @return the number
+     */
     @Override
     public Number parse(String str) {
         if (Objects.isNull(str)) {
